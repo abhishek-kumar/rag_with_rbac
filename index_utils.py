5ca941aaeea13011804e5cbba4fd0f660ac21f4a
Abhishek Kumar <abhishek.kumar.ak@gmail.com>
2024-06-12 21:55:31 +0000

    Handle files that are shared publicly (anyoneWithLink) instead of with individual users or groups.

--- index_utils.py
+++ index_utils.py
@@ -134,11 +134,14 @@ def read_documents(
     if "permissions" in item:
       for permission in item["permissions"]:
         if "displayName" not in permission:
+          if "id" in permission and permission["id"] == "anyoneWithLink":
+            read_access = ["all_users"]
+            break
           logging.error(
             f"Received {permission=} from Drive API without displayName in it. "
             "Check if you have authorized scopes to read this data.")
           continue
-        read_access.append(permission['displayName'])
+        read_access.append(permission["displayName"])
     if not read_access:
       # Assumption: if item does not have permissions, it is a public document.
       read_access = ["all_users"]
