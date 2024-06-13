20ac681ed2faff17ffaed93dd82ead3de9707bd4
Abhishek Kumar <abhishek.kumar.ak@gmail.com>
2024-06-13 08:38:19 +0000

    Check for changes before showing the response to the user.

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ from pinecone import Pinecone
 
 import index_utils
 
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 
 # OpenAIEmbeddings model.
@@ -101,6 +101,63 @@ def get_vectorstore_indexwrapper(
     vector_store = PineconeVectorStore(index, embed.embed_query, "text")
     return VectorStoreIndexWrapper(vectorstore=vector_store)
 
+def check_for_updates(
+        sources: List[str],
+        latest_documents: List[index_utils.Document],
+        existing_index_manifest: index_utils.IndexManifest) -> Optional[str]:
+    """
+    Checks if the sources of an LLM's response are out of date w.r.t. Google Drive.
+    If up to date, returns None.
+    Otherwise, returns a useful message for the user.
+    """
+    try:
+        
+        logging.info(f"check_for_updates: Fetched {len(latest_documents)} documents from Google Drive.")
+        new_manifest = {doc.file_id: doc for doc in latest_documents}
+        to_be_deleted, to_be_added, to_be_updated = index_utils.compare_index_manifests(
+            previous_manifest=existing_index_manifest,
+            new_manifest=new_manifest)
+        logging.info(
+            "check_for_updates: Comparing the index to latest document metadata, "
+            "the following updates are necessary:\n"
+            f"  - {len(to_be_deleted)} documents to be deleted.\n"
+            f"  - {len(to_be_added)} new documents to be added.\n"
+            f"  - {len(to_be_updated)} documents to be updated.")
+        message: str = ""
+        files_modified = set()
+        source_set = set(sources)
+        files_modified = set([file.name for file in to_be_deleted.values()])
+        sources_modified = set(source_set).intersection(files_modified)
+        if sources_modified:
+            message += (
+                "My response is drawn from deleted sources "
+                f"{', '.join(sources_modified)}, "
+                "Since my response is stale, I need to "
+                "my data before giving you an accurate response. ")
+        files_modified = set([file.name for file in to_be_added.values()])
+        if files_modified:
+            message += (
+                "There are new file(s) that have been recently added "
+                "which might have a better response to your query, "
+                "but I haven't processed them yet: "
+                f"{', '.join(files_modified)}. ")
+        files_modified = set([file.name for file in to_be_updated.values()])
+        sources_modified = set(source_set).intersection(files_modified)
+        if sources_modified:
+            message += (
+                "My response is drawn from stale sources "
+                f"{', '.join(sources_modified)}, which have been modified recently. "
+                "Since my response is stale, I need to "
+                "my data before giving you a more accurate response. ")
+        if message:
+            logging.info(message)
+            return message
+        return None
+    except Exception as ex:
+        logging.error(ex)
+        logging.error(traceback.format_exc())
+        raise Exception(f"Unable to check provenance of the sources in the LLM's response. {ex}.")
+
 def query_rag_with_rbac(
         input_text: str,
         llm: OpenAI,
@@ -127,6 +184,42 @@ def query_rag_with_rbac(
     logging.warning(f"DEBUG: query_rag_with_rbac: sources:\n{sources}")
     return (answer, sources)
 
+def query_rag_and_check_drive_for_updates(
+        input_text: str,
+        llm: OpenAI,
+        index: VectorStoreIndexWrapper,
+        groups: List[str],
+        pinecone_api_key:str ,
+        pinecone_index_name: str,
+        google_drive_root_folder_id: str) -> Tuple[str, List[str], index_utils.IndexManifest, Optional[str], List[index_utils.Document]]:
+    answer: str
+    sources: List[str]
+    index_manifest: index_utils.IndexManifest
+    latest_documents: List[index_utils.Document]
+    updates: Optional[str]
+
+    with st.spinner(text="Invoking LLM..."):
+        answer, sources = query_rag_with_rbac(
+            input_text=input_text,
+            llm=llm,
+            index=index,
+            groups=groups)
+    st.success(body="Successfully retrieved a response from the LLM.")
+
+    with st.spinner(f"Analyzing sources {', '.join(sources)}"):
+        index_manifest = index_utils.get_index_manifest(
+            pinecone_api_key=pinecone_api_key,
+            pinecone_index_name=pinecone_index_name)
+        latest_documents = index_utils.read_documents(
+                folder_id=google_drive_root_folder_id,
+                drive_service=st.session_state.drive_service,
+                include_files_with_extensions=["pdf"])
+        updates = check_for_updates(
+            sources=sources,
+            latest_documents=latest_documents,
+            existing_index_manifest=index_manifest)
+    st.success(body="Done analyzing sources.")
+    return (answer, sources, index_manifest, updates, latest_documents)
 
 def main():
     st.title('LLM App: RAG with RBAC v0.2')
@@ -194,61 +287,42 @@ def main():
         submitted = st.form_submit_button(label="Submit")
         if submitted and llm and index:
             try:
-                answer, sources = query_rag_with_rbac(
+                answer, sources, index_manifest, updates, latest_documents = query_rag_and_check_drive_for_updates(
                     input_text=text,
                     llm=llm,
                     index=index,
-                    groups=collected_groups)
-                # BEGIN: debug block (to be removed)
-                try:
-                    logging.warning(f"Attempting to read files from drive.")
-                    documents = index_utils.read_documents(
-                        folder_id=google_drive_root_folder_id,
-                        drive_service=st.session_state.drive_service,
-                        include_files_with_extensions=["pdf"])
-                    logging.warning(f"DEBUG: fetched {len(documents)} documents from Google Drive.")
-                    logging.info(f"{documents}")
-                    new_manifest: index_utils.IndexManifest = {doc.file_id: doc for doc in documents}
-                    to_be_deleted, to_be_added, to_be_updated = index_utils.compare_index_manifests(
-                        previous_manifest=index_utils.get_index_manifest(
+                    groups=collected_groups,
+                    pinecone_api_key=pinecone_api_key,
+                    pinecone_index_name=pinecone_index_name,
+                    google_drive_root_folder_id=google_drive_root_folder_id)
+                num_index_updates = 0
+                while updates is not None:
+                    if num_index_updates >= 2:
+                        raise Exception(
+                            f"Too many frequent and recent updates to files "
+                            "in Google Drive. Please check back later after "
+                            "all changes are done.")
+                    st.markdown(
+                        body=f"**:blue[Copilot:]** *{updates}*\n\n",
+                        help="Response from the LLM with RAC, applying role based access controls.")
+                    with st.spinner("Updating the index with recent file changes..."):
+                        docs_updated = index_utils.update_index_with_latest_documents(
+                            latest_documents=latest_documents,
+                            drive_service=st.session_state.drive_service,
                             pinecone_api_key=pinecone_api_key,
-                            pinecone_index_name=pinecone_index_name),
-                        new_manifest=new_manifest)
-                    logging.info(
-                        "Comparing the index to latest document metadata, "
-                        "the following updates are necessary:\n"
-                        f"  - {len(to_be_deleted)} documents to be deleted {list(to_be_deleted.values())}\n"
-                        f"  - {len(to_be_added)} new documents to be added {list(to_be_added.values())}\n"
-                        f"  - {len(to_be_updated)} documents to be updated {list(to_be_updated.values())}")
-                    files_modified = set()
-                    source_set = set(sources)
-                    files_modified = set([file.name for file in to_be_deleted.values()])
-                    sources_modified = set(source_set).difference(files_modified)
-                    if sources_modified:
-                        logging.warning(
-                            "My response is drawn from deleted sources "
-                            f"{', '.join(sources_modified)}, "
-                            "Since my response is stale, I need to "
-                            "my data before giving you an accurate response.")
-                    files_modified = set([file.name for file in to_be_added.values()])
-                    if files_modified:
-                        logging.warning(
-                            "There are new file(s) that have been recently added "
-                            "which might have a better response to your query, "
-                            "but I haven't processed them yet: "
-                            f"{', '.join(files_modified)}, ")
-                    files_modified = set([file.name for file in to_be_updated.values()])
-                    sources_modified = set(source_set).difference(files_modified)
-                    if sources_modified:
-                        logging.warning(
-                            "My response is drawn from stale sources "
-                            f"{', '.join(sources_modified)}, which have been modified recently. "
-                            "Since my response is stale, I need to "
-                            "my data before giving you a more accurate response.")
-                except Exception as ex:
-                    logging.error(ex)
-                    logging.error(traceback.format_exc())
-                # END: debug block (to be removed)
+                            pinecone_index_name=pinecone_index_name,
+                            existing_index_manifest=index_manifest,
+                            write_index_manifest_after_update=True)
+                        num_index_updates += 1
+                    st.success(f"Updated the index. {docs_updated=}.")
+                    answer, sources, index_manifest, updates, latest_documents = query_rag_and_check_drive_for_updates(
+                        input_text=text,
+                        llm=llm,
+                        index=index,
+                        groups=collected_groups,
+                        pinecone_api_key=pinecone_api_key,
+                        pinecone_index_name=pinecone_index_name,
+                        google_drive_root_folder_id=google_drive_root_folder_id)
                 response_markdown = f"**:blue[Copilot:]** *{answer}*\n\n"
                 response_markdown += f"**:blue[Sources]**\n\n"
                 for source in sources:
@@ -257,7 +331,7 @@ def main():
                     response_markdown += f"  :blue-background[None found.]\n"                
                 st.markdown(
                     body=response_markdown,
-                    help="Response from the LLM with RAC, applying role based access controls.")
+                    help="Response from the LLM with RAC, applying role based access controls.")                
             except Exception as ex:
                 st.warning(f"LLM/index error: {str(ex)}")
                 logging.warning(traceback.format_exc())
